e82327bf52fce3cc167c09f60c897732361e19ba
LuisMonsibais <luis.ramirez@bykon.com.mx>
2025-04-15 11:45:58 -0600

    Commit_New
    Cerrar sesión

--- lib/APIService/api_service.dart
+++ lib/APIService/api_service.dart
@@ -4,8 +4,7 @@ import 'package:mi_app/Object/User.dart';
 import '../Common/commonFunctions.dart';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 
-
-Future<void> saveToken(String key,String accessToken) async {
+Future<void> saveToken(String key, String accessToken) async {
   final storage = FlutterSecureStorage();
   try {
     await storage.write(key: key, value: accessToken);
@@ -17,7 +16,7 @@ Future<void> saveToken(String key,String accessToken) async {
 Future<String?> getToken(String key) async {
   final storage = FlutterSecureStorage();
   try {
-    final token = await storage.read(key : key);//'access_token'
+    final token = await storage.read(key: key);
     return token;
   } catch (e) {
     return null;
@@ -29,31 +28,30 @@ Future<void> deleteToken(String key) async {
   await storage.delete(key: key);
 }
 
-
-
 class ApiService {
   Future<void> saveBoolean(String key, bool value) async {
-  final storage = FlutterSecureStorage();
-  try {
-    // Convertir el booleano a String antes de guardarlo
-    await storage.write(key: key, value: value.toString());
-  } catch (e) {
-    return;
+    final storage = FlutterSecureStorage();
+    try {
+      await storage.write(key: key, value: value.toString());
+    } catch (e) {
+      return;
+    }
   }
-}
-Future<bool?> getBoolean(String key) async {
-  final storage = FlutterSecureStorage();
-  try {
-    final value = await storage.read(key: key);
-    if (value != null) {
-      return value.toLowerCase() == 'true'; // Convertir el String a boolean
-    } else {
+
+  Future<bool?> getBoolean(String key) async {
+    final storage = FlutterSecureStorage();
+    try {
+      final value = await storage.read(key: key);
+      if (value != null) {
+        return value.toLowerCase() == 'true';
+      } else {
+        return null;
+      }
+    } catch (e) {
       return null;
     }
-  } catch (e) {
-    return null;
   }
-}
+
   // Instancia única de la clase (Singleton)
   static final ApiService _instance = ApiService._internal();
 
@@ -65,14 +63,12 @@ Future<bool?> getBoolean(String key) async {
     return _instance;
   }
 
-  // URL base de la API
-
   // Servicio 1: Login
   Future<Map<String, dynamic>?> login(String email, String password) async {
     final url = CommonFunctions.validateUrl('/api/auth/v1/login');
     try {
       final response = await http.post(
-        Uri.parse(url),//url as Uri,
+        Uri.parse(url),
         headers: {'Content-Type': 'application/json'},
         body: jsonEncode({
           "email": email,
@@ -81,30 +77,32 @@ Future<bool?> getBoolean(String key) async {
       );
 
       if (response.statusCode == 200) {
-         final Map<String, dynamic> responseData = jsonDecode(response.body);
-         //-------------------------------------------------------------------
-          // Guardar los pricipales datos del usuario en la clase User
-                User.initialize(
-                  responseData['user']['area_name'],
-                  responseData['user']['email'],
-                  responseData['user']['full_name'], 
-                  responseData['user']['job_position'],
-                  //true,
-                  responseData['user']['remember_change_password'], 
-                  responseData['user']['admission_date'],
-                  responseData['user']['area_lead'], 
-                  responseData['access_token'],
-                  responseData['refresh_token']);
-                 // guardar el accessToken, refreshToken y Login por primera vez
-                  await saveBoolean('remember_change_password', responseData['user']['remember_change_password']);                 
-                  await saveToken('access_token',responseData['access_token']);
-                  await saveToken('refresh_token',responseData['refresh_token']);
-         //-------------------------------------------------------------------
-        return responseData; //data;
-      } else {return {"error":response.statusCode}; } 
-  } catch (e) {
-    return {"error": 500};
-  }
+        final Map<String, dynamic> responseData = jsonDecode(response.body);
+
+        User.initialize(
+          responseData['user']['area_name'],
+          responseData['user']['email'],
+          responseData['user']['full_name'],
+          responseData['user']['job_position'],
+          responseData['user']['remember_change_password'],
+          responseData['user']['admission_date'],
+          responseData['user']['area_lead'],
+          responseData['access_token'],
+          responseData['refresh_token'],
+        );
+
+        await saveBoolean(
+            'remember_change_password', responseData['user']['remember_change_password']);
+        await saveToken('access_token', responseData['access_token']);
+        await saveToken('refresh_token', responseData['refresh_token']);
+
+        return responseData;
+      } else {
+        return {"error": response.statusCode};
+      }
+    } catch (e) {
+      return {"error": 500};
+    }
   }
 
   // Servicio 2: Reset Password
@@ -123,19 +121,19 @@ Future<bool?> getBoolean(String key) async {
         final responseData = jsonDecode(response.body);
         return responseData;
       } else {
-        return {"error":response.statusCode};
+        return {"error": response.statusCode};
       }
     } catch (e) {
       return {"error": 500};
     }
   }
-    // Servicio 3: Create Password From ResetPassword
-    Future<Map<String, dynamic>?> passwordChange(String token, String userCode, String newPassword) async {
-    final url = CommonFunctions.validateUrl('/api/reset-password/v1/validate-change');
 
+  // Servicio 3: Create Password From ResetPassword
+  Future<Map<String, dynamic>?> passwordChange(String token, String userCode, String newPassword) async {
+    final url = CommonFunctions.validateUrl('/api/reset-password/v1/validate-change');
     try {
       final response = await http.post(
-         Uri.parse(url),//url as Uri,
+        Uri.parse(url),
         headers: {
           'Content-Type': 'application/json',
         },
@@ -150,25 +148,22 @@ Future<bool?> getBoolean(String key) async {
         final data = jsonDecode(response.body);
         return data;
       } else {
-         return {"error":response.statusCode};
+        return {"error": response.statusCode};
       }
     } catch (e) {
       return {"error": 500};
     }
   }
 
-    // Servicio 4: Generate-code From Cambiar Password
-
-    Future<Map<String, dynamic>?> generateCodeChangePassword() async {
+  // Servicio 4: Generate-code From Cambiar Password
+  Future<Map<String, dynamic>?> generateCodeChangePassword() async {
     final url = CommonFunctions.validateUrl('/api/change-password/v1/generate-code');
-
-      final accessToken = await getToken('access_token');
-
+    final accessToken = await getToken('access_token');
     try {
       final response = await http.get(
         Uri.parse(url),
         headers: {
-          'Authorization': 'Bearer $accessToken', //'Authorization': 'Bearer $authBearerToken',getToken(String key)
+          'Authorization': 'Bearer $accessToken',
           'Content-Type': 'application/json',
         },
       );
@@ -177,51 +172,47 @@ Future<bool?> getBoolean(String key) async {
         final data = jsonDecode(response.body);
         return data;
       } else {
-        return {"error":response.statusCode};
+        return {"error": response.statusCode};
       }
     } catch (e) {
       return {"error": 500};
     }
   }
-  // Servicio 5: Validate & change From Cambiar Password
-      Future<Map<String, dynamic>?> validateChangePassword(
-          //required String authToken,
-           String token,
-           String userCode,
-           String newPassword,
-        ) async {
-          final url = CommonFunctions.validateUrl('/api/change-password/v1/validate-change');
-          final accessToken = await getToken('access_token');
 
-          try {
-            final response = await http.post(
-              Uri.parse(url),
-              headers: {
-                'Authorization': 'Bearer $accessToken',
-                'Content-Type': 'application/json',
-              },
-              body: jsonEncode({
-                "token": token,
-                "user_code": userCode,
-                "new_password": newPassword,
-              }),
-            );
-      
-            if (response.statusCode == 200) {
-              final data = jsonDecode(response.body);
-              return data;
-            } else {
-              return {"error":response.statusCode};
-            }
-          } catch (e) {
-            return {"error": 500};
-          }
-        }
- //Servicio 6 Incidents by user
+  // Servicio 5: Validate & Change Password
+  Future<Map<String, dynamic>?> validateChangePassword(
+      String token, String userCode, String newPassword) async {
+    final url = CommonFunctions.validateUrl('/api/change-password/v1/validate-change');
+    final accessToken = await getToken('access_token');
+    try {
+      final response = await http.post(
+        Uri.parse(url),
+        headers: {
+          'Authorization': 'Bearer $accessToken',
+          'Content-Type': 'application/json',
+        },
+        body: jsonEncode({
+          "token": token,
+          "user_code": userCode,
+          "new_password": newPassword,
+        }),
+      );
+
+      if (response.statusCode == 200) {
+        final data = jsonDecode(response.body);
+        return data;
+      } else {
+        return {"error": response.statusCode};
+      }
+    } catch (e) {
+      return {"error": 500};
+    }
+  }
+
+  // Servicio 6: Get All Incidents By User
   Future<List<Map<String, dynamic>>?> getAllIncidentsByUser() async {
     final url = CommonFunctions.validateUrl('/api/incidents/v1/user/allIncidents');
     final accessToken = await getToken('access_token');
-
     try {
       final response = await http.get(
         Uri.parse(url),
@@ -240,12 +231,12 @@ Future<bool?> getBoolean(String key) async {
     } catch (e) {
       return null;
     }
-  }   
-  // Servicio 7: get all incidents
+  }
+
+  // Servicio 7: Get All Incidents
   Future<List<Map<String, dynamic>>?> getAllIncidents() async {
     final url = CommonFunctions.validateUrl('/api/incidents/v1/all');
-    final accessToken = await getToken('access_token'); // Obtener el token almacenado
-
+    final accessToken = await getToken('access_token');
     try {
       final response = await http.get(
         Uri.parse(url),
@@ -267,8 +258,8 @@ Future<bool?> getBoolean(String key) async {
     }
   }
 
-// Servicio 8: Servicio request Incident
-Future<Map<String, dynamic>?> requestIncident({
+  // Servicio 8: Request Incident
+  Future<Map<String, dynamic>?> requestIncident({
     required int incidenceUuid,
     required String startDate,
     required String finalDate,
@@ -278,8 +269,7 @@ Future<Map<String, dynamic>?> requestIncident({
     required String fiscalPeriod,
   }) async {
     final url = CommonFunctions.validateUrl('/api/incidents/v1/user/requestIncident');
-       final accessToken = await getToken('access_token'); // Obtener el token almacenado
-
+    final accessToken = await getToken('access_token');
     try {
       final response = await http.post(
         Uri.parse(url),
@@ -308,4 +298,36 @@ Future<Map<String, dynamic>?> requestIncident({
       return null;
     }
   }
- }//class
+
+  // Servicio 9: Logout
+  Future<Map<String, dynamic>?> logout() async {
+    final url = CommonFunctions.validateUrl('/api/auth/v1/logout');
+    final accessToken = await getToken('access_token');
+    print('Access Token: $accessToken');
+
+    try {
+      final response = await http.post(
+        Uri.parse(url),
+        headers: {
+          'Authorization': 'Bearer $accessToken',
+          'Content-Type': 'application/json',
+        },
+      );
+
+      print('Response status: ${response.statusCode}');
+      print('Response body: ${response.body}');
+
+      if (response.statusCode == 200) {
+        final storage = FlutterSecureStorage();
+        await storage.delete(key: 'access_token');
+        await storage.delete(key: 'refresh_token');
+        return {"message": "Sesión cerrada con éxito"};
+      } else {
+        return {"error": response.statusCode};
+      }
+    } catch (e) {
+      print('Error en logout: $e');
+      return {"error": 400};
+    }
+  }
+}
--- lib/configuration.dart
+++ lib/configuration.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-// Importa tu pantalla de cambiar contraseña
+import '../APIService/api_service.dart';
 import 'changepassword.dart'; // Ajusta la ruta según tu estructura de archivos
 
 class ConfigurationScreen extends StatefulWidget {
@@ -14,10 +14,12 @@ class _ConfigurationScreenState extends State<ConfigurationScreen> {
   bool isBiometricEnabled = true;
   bool isNotificationsEnabled = true;
 
+  // Instancia del servicio API
+  final ApiService _apiService = ApiService();
+
   @override
   Widget build(BuildContext context) {
     // ------------------- PARÁMETROS DE ESTILO -------------------
-    // Degradado de fondo
     final LinearGradient backgroundGradient = const LinearGradient(
       colors: [
         Color(0xFF33004C), // Morado oscuro
@@ -27,22 +29,18 @@ class _ConfigurationScreenState extends State<ConfigurationScreen> {
       end: Alignment.bottomRight,
     );
 
-    // AppBar personalizado
     final Color appBarColor = Colors.black;
     final Color appBarIconColor = Colors.white;
-    final double appBarBottomRadius = 10.0; // Borde redondeado inferior
+    final double appBarBottomRadius = 10.0;
     final String appBarTitle = 'Configuración';
     final double appBarTitleSize = 20.0;
     final Color appBarTitleColor = Colors.white;
 
-    // **Espacio fijo** para que se vea el degradado
-    final double spaceBetweenAppBarAndContainer = 80.0; 
+    final double spaceBetweenAppBarAndContainer = 80.0;
 
-    // Contenedor negro (para las tarjetas)
-    final double containerTopRadius = 16.0;  
+    final double containerTopRadius = 16.0;
     final Color containerColor = Colors.black;
 
-    // Tarjetas
     final Color cardBackgroundColor = const Color(0xFF1C1C1C);
     final double cardBorderRadius = 8.0;
     final double cardPadding = 16.0;
@@ -51,13 +49,11 @@ class _ConfigurationScreenState extends State<ConfigurationScreen> {
     final Color textColor = Colors.white;
     final double textSize = 18.0;
 
-    // Switch
     final Color switchActiveColor = Colors.white;
     final Color switchActiveTrackColor = const Color(0xFF9E00FF);
     final Color switchInactiveThumbColor = Colors.white;
     final Color switchInactiveTrackColor = Colors.grey;
 
-    // Botón “Cerrar sesión”
     final Color buttonBackgroundColor = Colors.black;
     final Color buttonTextColor = const Color.fromARGB(255, 242, 224, 254);
     final double buttonFontSize = 18.0;
@@ -68,16 +64,13 @@ class _ConfigurationScreenState extends State<ConfigurationScreen> {
     // ------------------------------------------------------------
 
     return Scaffold(
-      // Quitamos color sólido para mostrar el degradado
       body: Container(
         decoration: BoxDecoration(
           gradient: backgroundGradient,
         ),
         child: Column(
           children: [
-            // ------------------------------------------------------------
-            // AppBar "Personalizado" con bordes redondeados abajo
-            // ------------------------------------------------------------
+            // AppBar personalizado
             ClipRRect(
               borderRadius: BorderRadius.only(
                 bottomLeft: Radius.circular(appBarBottomRadius),
@@ -111,14 +104,9 @@ class _ConfigurationScreenState extends State<ConfigurationScreen> {
               ),
             ),
 
-            // ------------------------------------------------------------
-            // Espacio fijo para que se vea el degradado
-            // ------------------------------------------------------------
             SizedBox(height: spaceBetweenAppBarAndContainer),
 
-            // ------------------------------------------------------------
             // Contenedor negro con bordes redondeados arriba
-            // ------------------------------------------------------------
             Expanded(
               child: Container(
                 decoration: BoxDecoration(
@@ -130,7 +118,6 @@ class _ConfigurationScreenState extends State<ConfigurationScreen> {
                 ),
                 child: Column(
                   children: [
-                    // Scroll para las tarjetas
                     Expanded(
                       child: SingleChildScrollView(
                         padding: const EdgeInsets.symmetric(
@@ -140,15 +127,13 @@ class _ConfigurationScreenState extends State<ConfigurationScreen> {
                         child: Column(
                           children: [
                             // Tarjeta 1: "Cambiar contraseña"
-                            // Envuelta en GestureDetector (o InkWell) para que sea clicable
                             GestureDetector(
                               onTap: () {
-                                // Navega a la pantalla de cambiar contraseña
                                 Navigator.push(
                                   context,
                                   MaterialPageRoute(
                                     builder: (context) =>
-                                        const ChangePasswordScreen(), 
+                                        const ChangePasswordScreen(),
                                   ),
                                 );
                               },
@@ -283,7 +268,69 @@ class _ConfigurationScreenState extends State<ConfigurationScreen> {
                             padding: EdgeInsets.all(buttonPadding),
                           ),
                           onPressed: () {
-                            // Maneja aquí la acción de cerrar sesión
+                            showDialog(
+                              context: context,
+                              builder: (BuildContext context) {
+                                return AlertDialog(
+                                  shape: RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(16.0),
+                                  ),
+                                  backgroundColor: containerColor,
+                                  title: Row(
+                                    children: [
+                                      Icon(Icons.info, color: iconColor),
+                                      const SizedBox(width: 8),
+                                      Text(
+                                        'Cerrar sesión',
+                                        style: TextStyle(color: textColor),
+                                      ),
+                                    ],
+                                  ),
+                                  content: Text(
+                                    '¿Quieres cerrar tu sesión en este dispositivo?',
+                                    style: TextStyle(color: textColor),
+                                  ),
+                                  actions: [
+                                    TextButton(
+                                      onPressed: () {
+                                        Navigator.of(context).pop();
+                                      },
+                                      child: Text(
+                                        'Cancelar',
+                                        style:
+                                            TextStyle(color: buttonTextColor),
+                                      ),
+                                    ),
+                                    TextButton(
+                                      onPressed: () async {
+                                        Navigator.of(context).pop();
+                                        final result =
+                                            await _apiService.logout();
+                                        if (result != null &&
+                                            result.containsKey('message')) {
+                                          Navigator.of(context)
+                                              .pushNamedAndRemoveUntil(
+                                                  '/login', (route) => false);
+                                        } else {
+                                          ScaffoldMessenger.of(context)
+                                              .showSnackBar(
+                                            const SnackBar(
+                                              content: Text(
+                                                  'Error al cerrar sesión. Inténtalo de nuevo.'),
+                                            ),
+                                          );
+                                        }
+                                      },
+                                      child: Text(
+                                        'Cerrar sesión',
+                                        style:
+                                            TextStyle(color: buttonTextColor),
+                                      ),
+                                    ),
+                                  ],
+                                );
+                              },
+                            );
                           },
                           child: Text(
                             'Cerrar sesión',
